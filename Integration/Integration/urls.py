c3b83c224bccbd16b1789ac7bbae6a409f447272
yonghax <melfice_hax@hotmail.com>
2016-07-04 02:06:59 +0700

    Init proj

---
+++ Integration/Integration/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for Integration.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', Integration.views.home, name='home'),
+    # url(r'^Integration/', include('Integration.Integration.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
