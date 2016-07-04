96755a8fd90d1752fe4bca0cf0f16bd398ccebe2
yonghax <melfice_hax@hotmail.com>
2016-07-04 11:56:40 +0700

    update views.py to implement post order

--- Integration/Api/views.py
+++ Integration/Api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from Api.models import SaleOrder, SaleOrderLine
+from Api.serializers import SaleOrderSerializer, SaleOrderLineSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def OrderSyncProcess(request):
+    """
+    Retreive and process order instance. 
+    """
+    if request.method == 'POST':
+      serializer = SaleOrderSerializer(data=request.data)
+      if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data, status=status.HTTP_201_CREATED)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
